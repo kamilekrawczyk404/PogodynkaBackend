6e4812ef4f2e26368436892a3be514a5150d3326
kamilekkrawczyk404 <kamilekkrawczyk404@gmail.com>
2025-04-10 22:20:51 +0200

    feat: user home location (adding and removing)

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const prisma = new PrismaClient();
@@ -11,6 +16,146 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
+});
+
+// Utils
+const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+const comparePassword = async (password, hashedPassword) => {
+  return await bcrypt.compare(password, hashedPassword);
+};
+
+const generateToken = (user) => {
+  const { id, name } = user;
+
+  return jwt.sign({ id, name }, process.env.AUTH_SECRET_KEY, {
+    expiresIn: "1h",
+  });
+};
+
+// Routes
+app.post("/api/auth/register", async (req, res) => {
+  try {
+    const { Login, Name, Password } = req.body;
+
+    const existingUser = await prisma.user.findUnique({
+      where: { login: Login },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ Login: "Login is already taken" });
+    }
+
+    const hashedPassword = await hashPassword(Password);
+
+    const user = await prisma.user.create({
+      data: {
+        login: Login,
+        name: Name,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/api/auth/login", async (req, res) => {
+  try {
+    const { Login, Password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { login: Login },
+      // select: {
+      // id: true,
+      // name: true,
+      // },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        //sent global error
+        error: "Invalid Credentials",
+      });
+    }
+
+    const isMatch = await comparePassword(Password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        //sent global error
+        error: "Invalid Credentials",
+      });
+    }
+
+    const token = generateToken(user);
+
+    res.json({ token, user });
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/api/user/history/add", async (req, res) => {
+  const { weatherData, user } = req.body;
+
+  try {
+    const searchHistory = await prisma.searchHistory.create({
+      data: {
+        userId: user.id,
+        weatherData,
+      },
+    });
+
+    res.status(200).json({ searchHistory });
+    res.status(200).json({ error: "User doesn't have home location" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/api/user/home/remove", async (req, res) => {
+  const { user } = req.body;
+
+  try {
+    await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        homeLocation: null,
+      },
+    });
+    res.status(200).json({ message: "Home location removed successfully" });
+  } catch (error) {
+    console.warn(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+app.post("/api/user/home/set", async (req, res) => {
+  const { city, user } = req.body;
+
+  try {
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        homeLocation: city,
+      },
+    });
+
+    res.status(200).json({ updatedUser });
+  } catch (error) {
+    console.warn(error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 const apiKey = "d855022feded1934f8590f123da7b216";
